5f15701e7cf8cbea89ffd29483d88e80cf359aab
ni8mr <nilpaddo@gmail.com>
2016-09-03 13:27:10 +0600

    Add a program for user interaction

---
+++ interacting_with_user.py
@@ -0,0 +1,11 @@
+""" A program that does the following in order:
+    Asks the user to enter his/her date of birth.
+    Asks the user to enter his/her last name.
+    Prints out the user's last name and date of birth, in that order. """ 
+
+""" Problem set 0: Introduction to Computer Science and Programming """
+
+birth_date = raw_input("Enter your birth date: ")
+last_name = raw_input("Enter your last name: ")
+
+print(last_name + " has born at " + birth_date)
